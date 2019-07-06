0b2ac87b074e828debb8668f9a775c7740a310a0
nzh63 <nzh21@outlook.com>
2019-07-06 21:39:32 +0800

    添加测试

---
+++ Test/Test.cpp
@@ -0,0 +1,42 @@
+#include "CppUnitTest.h"
+#include <filesystem>
+
+#include "../MiniSysAssembler/pch.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test {
+TEST_CLASS(MiniSysAssembler){
+	private:
+		bool isSameFile(std::string path1, std::string path2) {
+			std::fstream file1, file2;
+			file1.open(path1);
+			file2.open(path2);
+			Assert::IsTrue(file1.is_open());
+			Assert::IsTrue(file2.is_open());
+			while (!file1.eof()) {
+	        	   std::string line1, line2;
+				   getline(file1, line1);
+				   getline(file2, line2);
+                   if (line1 != line2) return false;
+	        }
+			return true;
+		}
+
+    public:
+        TEST_METHOD(doAssemble_Test){
+            std::string out_path = std::filesystem::temp_directory_path().string();
+            Assert::IsFalse(
+                doAssemble("../../Test/TestData/test.asm", out_path));
+            Assert::IsTrue(
+                isSameFile(out_path + "dmem32.coe",
+                           "../../Test/TestData/CorrectAnswer/dmem32.coe"));
+            Assert::IsTrue(
+                isSameFile(out_path + "prgmip32.coe",
+                           "../../Test/TestData/CorrectAnswer/prgmip32.coe"));
+            std::filesystem::remove(out_path + "dmem32.coe");
+            std::filesystem::remove(out_path + "prgmip32.coe");
+        }
+
+};
+}
---
+++ Test/TestData/test.asm
@@ -0,0 +1,53 @@
+.data
+    AAA:    .word   0x3F9409, -6:6
+    BBB:    .word   0x123  :  3
+            .half   0x456, 3
+            .byte   -9, 10, 11, 12
+    XXX:    .word   0x1, 0x999
+    YYY:    .word   123
+    ZZZ:    .word   0
+            .word   16
+.text
+start:
+    mov     $1, AAA($0)
+    lw      $1, AAA($0)
+    mov     AAA($0), $1
+    sw      $1, AAA($0)
+    push    $1
+    pop     $1
+    add     $1, $2, $3
+    addu    $1, $2, $3
+    sub     $1, $2, $3
+    subu    $1, $2, $3
+    and     $1, $2, $3
+    or      $1, $2, $3
+    xor     $1, $2, $3
+    nor     $1, $2, $3
+    slt     $1, $2, $3
+    sltu    $1, $2, $3
+    sll     $1, $2, 10
+    srl     $1, $2, 10
+    sra     $1, $2, 10
+    sllv    $1, $2, $3
+    srlv    $1, $2, $3
+    srav    $1, $2, $3
+    jr      $31
+    addi    $1, $2, 0x10
+    addiu   $1, $2, -10
+    andi    $1, $2, 10
+    ori     $1, $2, 0x10
+    xori    $1, $2, 10
+    lui     $1, 10
+    lw      $1, 10($2)
+    sw      $1, AAA($2)
+    beq     $1, $2, 40
+    beq     $1, $2, start
+    bne     $1, $2, 40
+    bne     $1, $2, start
+    slti    $1, $2, 10
+    sltiu   $1, $2, 10
+    j       100
+    j       start
+    jal     100
+    jal     start
+
